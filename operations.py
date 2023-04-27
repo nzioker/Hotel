e8bf78db7fa4ec366fdadce6b3a55bacbce043e7
ken.kalima <kenneth@pondus.xyz>
2023-04-27 16:13:54 +0300

    Refactored the use of input and generation of room numbers

--- operations.py
+++ operations.py
@@ -16,34 +16,30 @@ class Rooms:
     def available_rooms(self):
         print(self.total_available_rooms)
 
+    def room_number(self, x,y):
+        room = random.randint(x, y)
+        if room in self.booked_rooms:
+            new_room = random.randint(x,y)
+            return new_room
+        return room
+    
+    def take_input(self, prompt):
+        u = input(prompt)
+        return u
+
+
     def generate_room_numbers(self, tier):
         if tier == "Presidential":
-            room = random.randint(1, 25)
-            if room in self.booked_rooms:
-                new_room = random.randint(1, 25)
-                return new_room
-            return room
+            return self.room_number(1,25)
 
         if tier == "Platinum":
-            room = random.randint(26, 55)
-            if room in self.booked_rooms:
-                new_room = random.randint(26, 55)
-                return new_room
-            return room
+            return self.room_number(26,55)
 
         if tier == "Gold":
-            room = random.randint(56, 105)
-            if room in self.booked_rooms:
-                new_room = random.randint(56, 105)
-                return new_room
-            return room
+            return self.room_number(56,105)
 
         if tier == "Ivory":
-            room = random.randint(106, 155)
-            if room in self.booked_rooms:
-                new_room = random.randint(106, 155)
-                return new_room
-            return room
+            return self.room_number(106,155)
 
     def all_booked_rooms(self):
         return self.booked_rooms
@@ -65,39 +61,25 @@ class Rooms:
             print(f"Proceed to room {self.room_number}. Here are your keys ")
 
         if payment < self.total_cost:
-            new_selection = input(
-                f"Unfortunately, Your payment is insufficient. Select one of these other tiers {self.remaining_tiers}"
-            )
+            new_selection = self.take_input(f"Unfortunately, Your payment is insufficient. Select one of these other tiers {self.remaining_tiers}")
             self.book_a_room(new_selection)
 
     def book_a_room(self, user_input):
-        # check availability of that tier
         self.remaining_tiers = list(self.rooms.keys())
         self.remaining_tiers.remove(user_input)
         if self.rooms[user_input] > 0:
-            self.number_of_rooms = int(
-                input(f"How many rooms do you want to pay for? ")
-            )
-
+            self.number_of_rooms = int(self.take_input("How many rooms do you want to pay for? "))
             if self.number_of_rooms < self.rooms[user_input]:
-                number_of_nights = int(
-                    input(
-                        f"Room is available. How many nights do you want to pay for? It costs USD {self.cost[user_input]} per night. "
-                    )
-                )
+                number_of_nights = int( self.take_input(f"Room is available. How many nights do you want to pay for? It costs USD {self.cost[user_input]} per night. "))
                 self.total_cost = (
                     number_of_nights * self.cost[user_input] * self.number_of_rooms
                 )
-                payment = int(input(f"Kindly make payment of USD {self.total_cost} "))
+                payment = int(self.take_input(f"Kindly make payment of USD {self.total_cost} "))
                 self.room_number = self.generate_room_numbers(user_input)
                 self.booked_rooms.append(self.room_number)
                 self.process_payments(payment, user_input)
             else:
-                new_selection = input(
-                    f"We're sorry. The cannot book this number of rooms for this tier. Select one of these other tiers {self.remaining_tiers}"
-                )
+                new_selection = self.take_input(f"We're sorry. The cannot book this number of rooms for this tier. Select one of these other tiers {self.remaining_tiers}")
         else:
-            new_selection = input(
-                f"Unfortunately, all our rooms for that tier are booked. Select one of these other tiers {self.remaining_tiers}"
-            )
+            new_selection = self.take_input(f"Unfortunately, all our rooms for that tier are booked. Select one of these other tiers {self.remaining_tiers}")
             self.book_a_room(new_selection)
